0f0597729c3feef71fac98b1b9f4a5556177319b
Mirza <mirza.sabanovic@ipi-akademija.ba>
2025-01-10 00:00:19 +0100

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent, CommonModule],
+  providers: [AuthService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule, CommonModule, FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, HttpClientModule],
+  providers: [AuthService],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
@@ -18,20 +21,35 @@ export class NavComponent {
     password: '',
   };
 
+  constructor(private authService: AuthService) {}
+
   togglePopup() {
     this.isPopupOpen = !this.isPopupOpen;
   }
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
+
   closeMenu() {
     this.menuOpen = false;
   }
 
   login() {
     if (this.loginData.email && this.loginData.password) {
-      alert(`Uspješna prijava: ${this.loginData.email}`);
-      this.togglePopup();
+      this.authService
+        .login(this.loginData.email, this.loginData.password)
+        .subscribe(
+          (response: any) => {
+            console.log('Prijava uspješna', response);
+            alert('Uspješna prijava!');
+            this.togglePopup();
+          },
+          (error: any) => {
+            console.error('Greška pri prijavi', error);
+            alert('Neuspješna prijava. Provjeri podatke.');
+          }
+        );
     } else {
       alert('Molimo unesite email i lozinku.');
     }
